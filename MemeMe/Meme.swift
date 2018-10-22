0e5635ec4df5bdc2e71f44cde1e51b6f50c57f15
Marcos Contente <marcosvgcontente@correio.itau-unibanco.com.br>
2018-10-22 19:15:51 -0200

    Add meme model

---
+++ MemeMe/Meme.swift
@@ -0,0 +1,17 @@
+//
+//  Meme.swift
+//  MemeMe
+//
+//  Created by Marcos V G Contente on 22/10/18.
+//  Copyright © 2018 marcoscontente. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    var topText: String
+    var bottomText: String
+    var originalImage: UIImage
+    var memedImage: UIImage
+}
