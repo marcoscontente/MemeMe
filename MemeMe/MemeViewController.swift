1f1b1edeba1a9d527c2b4a2c401ab7e14a7c9183
Marcos Contente <marcosvgcontente@correio.itau-unibanco.com.br>
2018-10-22 19:49:36 -0200

    setting UI

--- MemeMe/MemeViewController.swift
+++ MemeMe/MemeViewController.swift
@@ -8,17 +8,66 @@
 
 import UIKit
 
-class MemeViewController: UIViewController {
+class MemeViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
 
-    @IBOutlet weak var actionButton: UIBarButtonItem!
+    //MARK: - Outlets
+    
+    @IBOutlet weak var shareButton: UIBarButtonItem!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
     @IBOutlet weak var topTextField: UITextField!
     @IBOutlet weak var bottomTextField: UITextField!
     @IBOutlet weak var imageView: UIImageView!
     
+    // MARK: - Properties
+    
+    var pickerViewController = UIImagePickerController()
+    
+    // MARK: - Initialize methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        startUISettings()
+        setTextFields()
+        setDelegates()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    
+    func setDelegates() {
+        pickerViewController.delegate = self
+        topTextField.delegate = self
+        bottomTextField.delegate = self
+    }
+    
+    // MARK: - Setting UI
+    
+    func startUISettings() {
+        shareButton.isEnabled = false
+        imageView.image = UIImage(named: "camera")
+    }
+    
+    func setTextFields() {
+        topTextField.text = "top"
+        bottomTextField.text = "bottom"
+        let attributes: [String: Any] = [
+            NSAttributedStringKey.strokeColor.rawValue: UIColor.black,
+            NSAttributedStringKey.foregroundColor.rawValue: UIColor.white,
+            NSAttributedStringKey.strokeWidth.rawValue: -3.0,
+            NSAttributedStringKey.font.rawValue: UIFont(name: "Impact", size: 40)!
+        ]
+        topTextField.defaultTextAttributes = attributes
+        bottomTextField.defaultTextAttributes = attributes
+        topTextField.autocapitalizationType = .allCharacters
+        bottomTextField.autocapitalizationType = .allCharacters
+        topTextField.borderStyle = .none
+        bottomTextField.borderStyle = .none
+        topTextField.adjustsFontSizeToFitWidth = true
+        bottomTextField.adjustsFontSizeToFitWidth = true
+        topTextField.textAlignment = .center
+        bottomTextField.textAlignment = .center
     }
     
     
