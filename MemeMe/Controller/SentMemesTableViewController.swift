9fa278c6f5f907c83ca4cd5025b16c1625c7c775
Marcos Contente <marcos.contente@zup.com.br>
2018-12-06 12:12:09 -0200

    Add SentMemesTableViewController class

---
+++ MemeMe/Controller/SentMemesTableViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SentMemesTableViewController.swift
+//  MemeMe
+//
+//  Created by Marcos Vinicius Goncalves Contente on 06/12/18.
+//  Copyright © 2018 marcoscontente. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SentMemesTableViewController: UITableViewController {
+    
+    
+}
