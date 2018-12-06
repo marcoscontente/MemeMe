81e3fcd1f87158a81bfd8f2d56b7464af81e218f
Marcos Contente <marcos.contente@zup.com.br>
2018-12-06 12:09:14 -0200

    Add MemeDetailViewController class

---
+++ MemeMe/Controller/MemeDetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MemeDetailViewController.swift
+//  MemeMe
+//
+//  Created by Marcos Vinicius Goncalves Contente on 06/12/18.
+//  Copyright © 2018 marcoscontente. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MemeDetailViewController: UIViewController {
+    
+    @IBOutlet weak var memedImageDetailView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
