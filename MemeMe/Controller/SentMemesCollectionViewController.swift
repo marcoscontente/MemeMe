93b2e3a6f69fa58d930a61c21426c9e41d2eb5ac
Marcos Contente <marcos.contente@zup.com.br>
2018-12-06 13:50:39 -0200

    Add SentMemesCollectionViewController class

---
+++ MemeMe/Controller/SentMemesCollectionViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SentMemesCollectionViewController.swift
+//  MemeMe
+//
+//  Created by Marcos Vinicius Goncalves Contente on 06/12/18.
+//  Copyright © 2018 marcoscontente. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SentMemesCollectionViewController: UICollectionViewController {
+    
+    
+    
+}
---
+++ MemeMe/View/MemeCollectionViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  MemeCollectionViewCell.swift
+//  MemeMe
+//
+//  Created by Marcos Vinicius Goncalves Contente on 06/12/18.
+//  Copyright © 2018 marcoscontente. All rights reserved.
+//
+
+import Foundation
