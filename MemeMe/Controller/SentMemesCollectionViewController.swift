d678b50271fc25fe1f36dd2dce98dde605f26c1e
Marcos Contente <marcos.contente@zup.com.br>
2018-12-07 16:40:45 -0200

    Set collectionViewController dataSource methods

--- MemeMe/Controller/SentMemesCollectionViewController.swift
+++ MemeMe/Controller/SentMemesCollectionViewController.swift
@@ -11,6 +11,33 @@ import UIKit
 
 class SentMemesCollectionViewController: UICollectionViewController {
     
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return appDelegate.memes.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MemeCollectionViewCell", for: indexPath) as! MemeCollectionViewCell
+        cell.memeImage.image = appDelegate.memes[indexPath.row].memedImage
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let detailController = storyboard?.instantiateViewController(withIdentifier: "MemeDetailViewController") as! MemeDetailViewController
+        detailController.memes = appDelegate.memes[indexPath.row]
+        self.navigationController?.pushViewController(detailController, animated: true)
+    }
+    
+    @IBAction func createNewMeme(_ sender: Any) {
+        let memeEditorController = storyboard?.instantiateViewController(withIdentifier: "MemeEditorViewController") as! MemeEditorViewController
+        if let navigationController = navigationController {
+            navigationController.pushViewController(memeEditorController, animated: true)
+        }
+    }
     
 }
